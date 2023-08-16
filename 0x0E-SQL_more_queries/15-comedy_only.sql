40baaeacd33602213c879f22ddc754762c22562d
Themba <thembaradebe172@gmail.com>
2023-08-16 11:44:19 -0700

    database sorting

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,8 +1,11 @@
 -- Lists all Comedy shows from the database
 -- Retrieves the show title from tv_shows table using INNER JOIN for corresponding genres
 -- Results are sorted in ascending order by show title
-SELECT tv_shows.title
+SELECT tv_shows.title AS title
 FROM tv_shows
-INNER JOIN tv_genres ON tv_shows.genre_id = tv_genres.id
+     JOIN tv_show_genres
+     	  ON tv_show_genres.show_id = tv_shows.id
+     JOIN tv_genres
+     	  ON tv_genres.id = tv_show_genres.genre_id
 WHERE tv_genres.name = 'Comedy'
-ORDER BY tv_shows.title ASC;
+ORDER BY title;
