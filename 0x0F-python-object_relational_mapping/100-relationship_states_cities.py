b32de36ba7652df11ebe88df0811c31996efedeb
Themba <thembaradebe172@gmail.com>
2023-09-17 20:07:33 -0700

    correction

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -10,32 +10,19 @@ from relationship_city import City
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-if __name__ == "__main__":
-    # Create a connection to the database
-    db_engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
-                              format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                              pool_pre_ping=True)
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
 
-    # Create a configured "Session" class
-    Session = sessionmaker(db_engine)
-
-    # Create all the tables in the database which are defined
-    # in your Base class (Base is defined in relationship_state.py)
-    Base.metadata.create_all(db_engine)
-
-    # Create a new session
+    Session = sessionmaker(bind=engine)
     session = Session()
 
-    # Create a new State and City objects
-    new_state = State(name="California")
-    new_city = City(name="San Francisco", state=new_state)
+    newState = State(name='California')
+    newCity = City(name='San Francisco')
+    newState.cities.append(newCity)
 
-    # Add the State and City objects to the session
-    session.add(new_state)
-    session.add(new_city)
-
-    # Commit the transaction
+    session.add(newState)
+    session.add(newCity)
     session.commit()
-
-    # Close the session
-    session.close()
